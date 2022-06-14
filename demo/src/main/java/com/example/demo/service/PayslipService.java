aaa446ea4516f72b1d6e81d5610494229605c6b1
Su Po Po Aung (s3689622) <43394993+supopoaung@users.noreply.github.com>
2022-06-15 05:40:25 +1000

    deleted

--- demo/src/main/java/com/example/demo/service/PayslipService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.Employee;
-import com.example.demo.entity.Payslip;
-
-public interface PayslipService {
-
-	Payslip preparePaySlip( Employee e);
-}
